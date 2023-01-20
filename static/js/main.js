ef59a5eca2e758f2d15f53b794ed4040f668e924
ohtani shimpei <ohtani.shimpei@gmail.com>
2023-01-21 00:44:20 +0900

    change js

--- static/js/main.js
+++ static/js/main.js
@@ -6,36 +6,35 @@ $(function(){
 });
 
 $(function(){
-   let $body = $('body');
-   $('.l-header__menu-btn').on('click', ()=>{
-   $body.toggleClass('side-open');
-   });
-   $('.l-header__menu').on('click', ()=>{
-   $body.removeClass('side-open');
-   });
+	let $body = $('body');
+	$('.l-header__menu-btn').on('click', ()=>{
+		$body.toggleClass('side-open');
+	});
+	$('.l-header__menu').on('click', ()=>{
+		$body.removeClass('side-open');
+	});
 });
 
 function PageTopAnime() {
-    var scroll = $(window).scrollTop();
-    if (scroll >= 200){
-      $('#page-top').removeClass('right-move');
-      $('#page-top').addClass('left-move');
-    }else{
-      if(
-        $('#page-top').hasClass('left-move')){
-        $('#page-top').removeClass('left-move');
-        $('#page-top').addClass('right-move');
-      }
-    }
+	let scroll = $(window).scrollTop();
+	if (scroll >= 200){
+		$('#page-top').removeClass('right-move');
+		$('#page-top').addClass('left-move');
+	} else {
+		if($('#page-top').hasClass('left-move')){
+			$('#page-top').removeClass('left-move');
+			$('#page-top').addClass('right-move');
+		}
+	}
 }
 
 $(window).scroll(()=>{
-  PageTopAnime();
+	PageTopAnime();
 });
 
 $('#page-top').click(()=>{
-    $('body,html').animate({
-        scrollTop: 0
-    }, 500);
-    return false;
+	$('body,html').animate({
+		scrollTop: 0
+	}, 500);
+	return false;
 });
