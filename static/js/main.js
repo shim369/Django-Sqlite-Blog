d150270928b18501f2772da0ec38d7a024633759
ohtani shimpei <ohtani.shimpei@gmail.com>
2023-02-26 08:31:43 +0900

    delete page-top & other

--- static/js/main.js
+++ static/js/main.js
@@ -14,27 +14,3 @@ $(function(){
 		$body.removeClass('side-open');
 	});
 });
-
-function PageTopAnime() {
-	let scroll = $(window).scrollTop();
-	if (scroll >= 200){
-		$('#page-top').removeClass('right-move');
-		$('#page-top').addClass('left-move');
-	} else {
-		if($('#page-top').hasClass('left-move')){
-			$('#page-top').removeClass('left-move');
-			$('#page-top').addClass('right-move');
-		}
-	}
-}
-
-$(window).scroll(()=>{
-	PageTopAnime();
-});
-
-$('#page-top').click(()=>{
-	$('body,html').animate({
-		scrollTop: 0
-	}, 500);
-	return false;
-});
