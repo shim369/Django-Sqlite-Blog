1d1198407c5a9f21899edf6bbf15c3a567e0451e
ohtani shimpei <ohtani.shimpei@gmail.com>
2023-06-24 17:31:00 +0900

    update js

--- static/js/bmi.js
+++ static/js/bmi.js
@@ -15,13 +15,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
         table.classList.add('p-article__table__bmi2');
 
         let bmiRanges = [
-            { min: -Infinity, max: 16.00, description: 'Severe thinness' },
-            { min: 16.00, max: 17.00, description: 'Moderate thinness' },
-            { min: 17.00, max: 18.50, description: 'Mild thinness' },
-            { min: 18.50, max: 25.00, description: 'Normal range' },
-            { min: 25.00, max: 30.00, description: 'Pre-obese' },
-            { min: 30.00, max: 35.00, description: 'Obese class1' },
-            { min: 35.00, max: 40.00, description: 'Obese class2' },
+            { min: -Infinity, max: 15.99, description: 'Severe thinness' },
+            { min: 16.00, max: 16.99, description: 'Moderate thinness' },
+            { min: 17.00, max: 18.49, description: 'Mild thinness' },
+            { min: 18.50, max: 24.99, description: 'Normal range' },
+            { min: 25.00, max: 29.99, description: 'Pre-obese' },
+            { min: 30.00, max: 34.99, description: 'Obese class1' },
+            { min: 35.00, max: 39.99, description: 'Obese class2' },
             { min: 40.00, max: Infinity, description: 'Obese class3' }
         ];
 
@@ -63,19 +63,28 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
         for (let i = 0; i < rows.length; i++) {
             let range = rows[i].cells[1].innerText.split(' - ');
-            let min = parseFloat(range[0]);
-            let max;
-            if (range[1] === '') {
+            let min, max;
+            if (range[0] === '-') {
+                min = -Infinity;
+                max = parseFloat(range[1]);
+            } else if (range[1] === '') {
+                min = parseFloat(range[0]);
                 max = Infinity;
             } else {
+                min = parseFloat(range[0]);
                 max = parseFloat(range[1]);
             }
 
             if (bmi >= min && bmi < max) {
                 rows[i].style.backgroundColor = 'lightgray';
+            } else if (bmi >= 40 && rows[i].cells[0].innerText.includes('Obese class3')) {
+                rows[i].style.backgroundColor = 'lightgray';
+            } else if (bmi <= 15.99 && rows[i].cells[0].innerText.includes('Severe thinness')) {
+                rows[i].style.backgroundColor = 'lightgray';
             } else {
                 rows[i].style.backgroundColor = '';
             }
+            
         }
     }
 
